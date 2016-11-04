71feb16d06c78eb6f1bb6b822461f2fa77d3e6bd
Wesley Ketchum <wketchum@fnal.gov>
2016-11-04 12:39:28 +0100

    shutdown/terminate commands

---
+++ bernfebdaq/tools/SDAQ_Terminate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source setup.sh
+
+HOST=`hostname -s`
+
+xmlrpc $HOST:5200/RPC2 pmt.stopSystem
+ssh ${HOST} "ipcs | grep ${shmKeyString} | awk '{print \$2}' | xargs ipcrm -m 2>/dev/null"
+xmlrpc $HOST:5200/RPC2 pmt.exit
